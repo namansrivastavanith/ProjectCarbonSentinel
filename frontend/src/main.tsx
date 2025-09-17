3d117be469ef4c6b87cff20412d255a74cdb9a32
Naman Srivastava <namansrivastavanith@gmail.com>
2025-09-17 18:36:56 +0530

    added pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter> {/* Wrap your App component */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ frontend/src/pages/Homepage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Carbon Sentinel</h1>
+      <p>Your personal carbon footprint tracker.</p>
+    </div>
+  );
+}
+
+export default HomePage;
